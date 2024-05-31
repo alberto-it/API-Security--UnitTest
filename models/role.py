6464872bd2b0b6b5b15d481b7abd25b33ab0ac60
alberto-it <alberto.it@gmail.com>
2024-05-31 14:09:00 -0400

    tweak

--- models/role.py
+++ models/role.py
@@ -6,4 +6,4 @@ class Role(Base):
     __tablename__ = "roles"
     id: Mapped[int] = mapped_column(primary_key=True)
     role_name: Mapped[str] = mapped_column(db.String(100), unique=True, nullable=False)
-    customers: Mapped[List["Customer"]] = db.relationship(back_populates='role')
+    customers: Mapped[List["Customer"]] = db.relationship(back_populates='roles')
