8f805eaa2709c2ed630a805f2f11a8d682ad6500
alberto-it <alberto.it@gmail.com>
2024-06-01 15:22:41 -0400

    deploy updates

--- database.py
+++ database.py
@@ -1,6 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+import os
+from flask_migrate import Migrate
 
 class Base(DeclarativeBase): pass
 
+MIGRATIONS_FOLDER = os.environ.get('MIGRATIONS_FOLDER') or 'migrations'
+
 db = SQLAlchemy(model_class=Base)
+migrate = Migrate(directory=MIGRATIONS_FOLDER)
