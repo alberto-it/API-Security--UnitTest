36cd82356ee4e3bc9ee1d1425a558f1289f496a2
alberto-it <alberto.it@gmail.com>
2024-06-01 13:32:30 -0400

    deploy updates

--- config.py
+++ config.py
@@ -4,3 +4,10 @@ class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     CACHE_TYPE = 'SimpleCache'
     DEBUG = True
+
+import os
+
+class ProductionConfig:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    CACHE_TYPE = 'SimpleCache'
+    DEBUG = False
