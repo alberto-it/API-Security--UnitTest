45f115b6e36590ddff7b4f2036ed377b4e8274d6
alberto-it <alberto.it@gmail.com>
2024-06-01 15:33:23 -0400

    deploy updates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_swagger_ui import get_swaggerui_blueprint
-from database import db
+from database import db, migrate
 from schemas import ma
 from limiter import limiter
 from caching import cache
